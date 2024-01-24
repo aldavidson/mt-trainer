7848952121339d491a30fa2c152fe89a30333e31
Al Davidson <apdavidson@gmail.com>
2024-01-24 10:39:25 +0000

    linting

--- src/frame_processor.py
+++ src/frame_processor.py
@@ -9,22 +9,31 @@ from text_rendering import Cv2TextRenderer, PILTextRenderer
 
 class FrameProcessor:
 
-  def __init__(self, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-    self.pose_landmarker = mp.solutions.pose.Pose(min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
+    def __init__(self, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.pose_landmarker = mp.solutions.pose.Pose(
+          min_detection_confidence=min_detection_confidence,
+          min_tracking_confidence=min_tracking_confidence
+        )
     
-  def pose_landmarks(self, rgb_image):
-    return self.pose_landmarker.process(rgb_image)
+    def quantify_pose(self, rgb_image):
+        results = self.pose_landmarker.process(rgb_image)
+        quant_pose = QuantifiedPose(results.pose_world_landmarks,
+                                    results.pose_landmarks)
+        # calculate key body angles
+        quant_pose.calculate_angles()
+        return quant_pose
 
-  def draw_landmarks(self, landmarks, rgb_image):
-    '''
-      Return a copy of the image with landmarks drawn & connected
-      Can only do this on a copy - the mp_image.numpy_view() is immutable
-    '''
-    rgb_image_copy = np.copy(rgb_image)
-    mp.solutions.drawing_utils.draw_landmarks(
-      rgb_image_copy,
-      landmarks, 
-      mp.solutions.pose.POSE_CONNECTIONS,
-      landmark_drawing_spec=mp.solutions.drawing_styles.get_default_pose_landmarks_style())
-    
-    return rgb_image_copy
+    def draw_landmarks(self, landmarks, rgb_image):
+        '''
+          Return a copy of the image with landmarks drawn & connected
+          Can only do this on a copy - the mp_image.numpy_view() is immutable
+        '''
+        rgb_image_copy = np.copy(rgb_image)
+        style = mp.solutions.drawing_styles.get_default_pose_landmarks_style()
+        mp.solutions.drawing_utils.draw_landmarks(
+          rgb_image_copy,
+          landmarks, 
+          mp.solutions.pose.POSE_CONNECTIONS,
+          landmark_drawing_spec=style)
+        
+        return rgb_image_copy
--- src/quantified_pose.py
+++ src/quantified_pose.py
@@ -2,81 +2,85 @@ import mediapipe as mp
 import vector_maths
 
 class QuantifiedPose:
-  mp_pose = mp.solutions.pose
+    mp_pose = mp.solutions.pose
   
-  ANGLE_LANDMARKS = {
-    "left_ankle_extension": (mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value,
-                  mp_pose.PoseLandmark.LEFT_ANKLE.value,
-                  mp_pose.PoseLandmark.LEFT_KNEE.value),
-    
-    "left_knee_extension": (mp_pose.PoseLandmark.LEFT_ANKLE.value,
-                  mp_pose.PoseLandmark.LEFT_KNEE.value,
-                  mp_pose.PoseLandmark.LEFT_HIP.value),
-    
-    "left_hip_extension": (mp_pose.PoseLandmark.LEFT_KNEE.value,
-                  mp_pose.PoseLandmark.LEFT_HIP.value,
-                  mp_pose.PoseLandmark.LEFT_SHOULDER.value),
-    
-    "left_hip_abduction": (mp_pose.PoseLandmark.LEFT_KNEE.value,
-                  mp_pose.PoseLandmark.LEFT_HIP.value,
-                  mp_pose.PoseLandmark.RIGHT_HIP.value),
-    
-    "left_shoulder_elevation": (mp_pose.PoseLandmark.LEFT_HIP.value,
-                  mp_pose.PoseLandmark.LEFT_SHOULDER.value,
-                  mp_pose.PoseLandmark.LEFT_ELBOW.value),
-  
-    "left_shoulder_abduction": (mp_pose.PoseLandmark.LEFT_ELBOW.value,
-                  mp_pose.PoseLandmark.LEFT_SHOULDER.value,
-                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
-    
-    "left_elbow_extension": (mp_pose.PoseLandmark.LEFT_WRIST.value,
-                  mp_pose.PoseLandmark.LEFT_ELBOW.value,
-                  mp_pose.PoseLandmark.LEFT_SHOULDER.value),
-    
-    "right_ankle_extension": (mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value,
-                  mp_pose.PoseLandmark.RIGHT_ANKLE.value,
-                  mp_pose.PoseLandmark.RIGHT_KNEE.value),
-    
-    "right_knee_extension": (mp_pose.PoseLandmark.RIGHT_ANKLE.value,
-                  mp_pose.PoseLandmark.RIGHT_KNEE.value,
-                  mp_pose.PoseLandmark.RIGHT_HIP.value),
-    
-    "right_hip_extension": (mp_pose.PoseLandmark.RIGHT_KNEE.value,
-                  mp_pose.PoseLandmark.RIGHT_HIP.value,
-                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
-    
-    "right_hip_abduction": (mp_pose.PoseLandmark.RIGHT_KNEE.value,
-                  mp_pose.PoseLandmark.RIGHT_HIP.value,
-                  mp_pose.PoseLandmark.LEFT_HIP.value),
-    
-    "right_shoulder_elevation": (mp_pose.PoseLandmark.RIGHT_HIP.value,
-                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
-                  mp_pose.PoseLandmark.RIGHT_ELBOW.value),
-  
-    "right_shoulder_abduction": (mp_pose.PoseLandmark.RIGHT_ELBOW.value,
-                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
-                  mp_pose.PoseLandmark.LEFT_SHOULDER.value),
-    
-    "right_elbow_extension": (mp_pose.PoseLandmark.RIGHT_WRIST.value,
-                  mp_pose.PoseLandmark.RIGHT_ELBOW.value,
-                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
-  }
+    ANGLE_LANDMARKS = {
+        "left_ankle_extension": (mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value,
+                                 mp_pose.PoseLandmark.LEFT_ANKLE.value,
+                                 mp_pose.PoseLandmark.LEFT_KNEE.value),
 
+        "left_knee_extension": (mp_pose.PoseLandmark.LEFT_ANKLE.value,
+                                mp_pose.PoseLandmark.LEFT_KNEE.value,
+                                mp_pose.PoseLandmark.LEFT_HIP.value),
 
-  def __init__(self, world_landmarks, image_landmarks, angles={}):
-    self.world_landmarks = world_landmarks
-    self.image_landmarks = image_landmarks
-    if bool(angles):
-      self.angles = angles
-    else:
-      self.angles = self.calculate_angles()
-  
-  def calculate_angles(self):
-    angles = {}
-    for angle in self.ANGLE_LANDMARKS:
-      landmarks = map( lambda l: vector_maths.landmark_to_vector(self.world_landmarks.landmark[l]), self.ANGLE_LANDMARKS[angle])
-      angles[angle] = vector_maths.angle_between( *landmarks )
-    return angles
+        "left_hip_extension": ( mp_pose.PoseLandmark.LEFT_KNEE.value,
+                                mp_pose.PoseLandmark.LEFT_HIP.value,
+                                mp_pose.PoseLandmark.LEFT_SHOULDER.value),
 
-  def rounded_angles(self):
-    return dict( (k, round(v,0)) for k,v in self.angles.items())
+        "left_hip_abduction": ( mp_pose.PoseLandmark.LEFT_KNEE.value,
+                                mp_pose.PoseLandmark.LEFT_HIP.value,
+                                mp_pose.PoseLandmark.RIGHT_HIP.value),
+
+        "left_shoulder_elevation": (mp_pose.PoseLandmark.LEFT_HIP.value,
+                                    mp_pose.PoseLandmark.LEFT_SHOULDER.value,
+                                    mp_pose.PoseLandmark.LEFT_ELBOW.value),
+
+        "left_shoulder_abduction": (mp_pose.PoseLandmark.LEFT_ELBOW.value,
+                                    mp_pose.PoseLandmark.LEFT_SHOULDER.value,
+                                    mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
+
+        "left_elbow_extension": ( mp_pose.PoseLandmark.LEFT_WRIST.value,
+                                  mp_pose.PoseLandmark.LEFT_ELBOW.value,
+                                  mp_pose.PoseLandmark.LEFT_SHOULDER.value),
+
+        "right_ankle_extension": (mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value,
+                                  mp_pose.PoseLandmark.RIGHT_ANKLE.value,
+                                  mp_pose.PoseLandmark.RIGHT_KNEE.value),
+
+        "right_knee_extension":  (mp_pose.PoseLandmark.RIGHT_ANKLE.value,
+                                  mp_pose.PoseLandmark.RIGHT_KNEE.value,
+                                  mp_pose.PoseLandmark.RIGHT_HIP.value),
+
+        "right_hip_extension": (mp_pose.PoseLandmark.RIGHT_KNEE.value,
+                                mp_pose.PoseLandmark.RIGHT_HIP.value,
+                                mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
+
+        "right_hip_abduction": (mp_pose.PoseLandmark.RIGHT_KNEE.value,
+                                mp_pose.PoseLandmark.RIGHT_HIP.value,
+                                mp_pose.PoseLandmark.LEFT_HIP.value),
+
+        "right_shoulder_elevation":  (mp_pose.PoseLandmark.RIGHT_HIP.value,
+                                      mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
+                                      mp_pose.PoseLandmark.RIGHT_ELBOW.value),
+
+        "right_shoulder_abduction":  (mp_pose.PoseLandmark.RIGHT_ELBOW.value,
+                                      mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
+                                      mp_pose.PoseLandmark.LEFT_SHOULDER.value),
+
+        "right_elbow_extension": (mp_pose.PoseLandmark.RIGHT_WRIST.value,
+                                  mp_pose.PoseLandmark.RIGHT_ELBOW.value,
+                                  mp_pose.PoseLandmark.RIGHT_SHOULDER.value),
+    }
+
+    def __init__(self, world_landmarks, image_landmarks, angles=None):
+        self.world_landmarks = world_landmarks
+        self.image_landmarks = image_landmarks
+        if angles is not None:
+            self.angles = angles
+        else:
+            self.angles = self.calculate_angles()
+   
+    def calculate_angles(self):
+        angles = {}
+        for angle, landmark_names in self.ANGLE_LANDMARKS.items():
+            landmarks = map( 
+                lambda l: vector_maths.landmark_to_vector(
+                            self.world_landmarks.landmark[l]
+                          ),
+                landmark_names
+            )
+            angles[angle] = vector_maths.angle_between(*landmarks)
+        return angles
+
+    def rounded_angles(self):
+        return dict((k, round(v, 0)) for k, v in self.angles.items())
