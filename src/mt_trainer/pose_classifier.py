57bd9d45a0d73f40f3ddba9c7d23365d657d458a
Al Davidson <apdavidson@gmail.com>
2024-02-06 10:14:36 +0000

    add pose_classifier, using basic cosine similarity on pose angles

---
+++ src/mt_trainer/pose_classifier.py
@@ -0,0 +1,60 @@
+import os
+import pathlib
+
+from json.decoder import JSONDecodeError
+
+from mt_trainer.quantified_pose import QuantifiedPose
+
+
+class PoseClassifier:
+  
+    def __init__(self, pose_archetypes=None, data_dir=None):
+        self.pose_archetypes = pose_archetypes or {}
+        if data_dir:
+            self.load_training_data(data_dir)
+
+    def load_training_data(self, dir):
+        for technique in QuantifiedPose.TECHNIQUES:
+            archetype = QuantifiedPose(None, None, {})
+            
+            technique_dir = os.path.join(dir, technique)
+            files = PoseClassifier.files_in(technique_dir)
+
+            files_loaded = 0
+            for file in files:
+                try:
+                    angles = QuantifiedPose().load_angles(file)
+                    files_loaded += 1
+                    for name, value in angles.items():
+                        archetype.angles[name] = (archetype.angles.get(name) or 0.0) 
+                        archetype.angles[name] += value
+                except JSONDecodeError:
+                    pass
+
+            if files_loaded > 1:
+                for name, value in archetype.angles.items():
+                    archetype.angles[name] /= files_loaded
+
+            self.pose_archetypes[technique] = archetype
+
+    @staticmethod
+    def files_in(dir):
+        return [
+            os.path.join(dirpath, f)
+            for (dirpath, dirnames, filenames) in os.walk(dir)
+            for f in filenames
+        ]
+
+    def similarities(self, pose):
+        similarities = {}
+        for technique, archetype in self.pose_archetypes.items():
+            similarities[technique] = pose.similarity_to(archetype)
+
+        return similarities
+
+    def classify(self, pose, threshold=0.9):
+        poses = self.similarities(pose)
+        poses_over_threshold = list(
+          (k, v) for k, v in poses.items() if v and v >= threshold
+        )
+        return sorted(poses_over_threshold, key=lambda v: v[1])
